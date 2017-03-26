8bd8288c2a251e8e802e83fcd607eb99d1b912d1
yangjingzhuo <yangjingzhuo@jiuan.com>
2017-03-26 23:25:22 +0800

    diving touch slider

--- dist/iceSkating.js
+++ dist/iceSkating.js
@@ -13,56 +13,120 @@ function iceSkating(option){
 
 	var container = document.querySelector(option.containerId);
 	var defaults = {
-		resistanceRatio: 0.4,
+		touchRatio: 0.6,
 		direction: 'x',
-
+		swipeRatio: 0.1,
+		animationDuration: 300
 	};
-	var id = option.containerId.substr(1);
+	var id = option.containerId.substr(1),
+	    swipeRatio = option.swipeRatio || defaults.swipeRatio,
+	 	childWidth = container.children[0].offsetWidth,
+		childHeight = container.children[0].offsetHeight;
+
 	store[id] = {
-		option: option,
 		children: container.children,
 		childLength: container.children.length,
-		childWidth: container.children[0].offsetWidth,
-		childHeight: container.children[0].offsetHeight,
-		index: 0
+		childWidth: childWidth,
+		childHeight: childHeight,
+		index: 0,
+		translateX: 0,
+		translateY: 0,
+		touchRatio: option.touchRatio || defaults.touchRatio,
+		direction: option.direction || defaults.direction,
+		swipeRatio: swipeRatio,
+		animationDuration: option.animationDuration || defaults.animationDuration,
+		limitDisX: swipeRatio * childWidth,
+		limitDisY: swipeRatio * childHeight
 	};
-	console.log(store)
+
 	var ic = this;
 
 	ic.touchStart = function(e){
-		if (!ic.support.touch && 'which' in e && e.which === 3) return;
-		
+		if (!ic.support.touch && 'which' in e && e.which === 3  || state.animating) return;
+		e.preventDefault();
+        e.stopPropagation();
 		state.startX = e.type === 'touchstart' ? e.targetTouches[0].pageX : e.pageX;
         state.startY = e.type === 'touchstart' ? e.targetTouches[0].pageY : e.pageY;
+		state.startTime = e.timeStamp;
 		state.currentTarget = e.currentTarget;
-		var currStore =  store[state.currentTarget.id];
-
 		state.target = e.target;
-		state.option = currStore.option;
-		state.resistanceRatio = state.option.resistanceRatio || defaults.resistanceRatio;
-		state.direction = state.option.direction || defaults.direction;
-		state.index = currStore.index;
+		state.currStore = store[e.currentTarget.id];
+		state.touchEnd = state.touchMove = false;
+		state.touchStart = true;
+		state.diffY = state.diffY = 0;
+
+		console.log('touchstart',state)
 	};
+
 	ic.touchMove = function(e){
-		if(e.target !== state.target) return;
+		if(e.target !== state.target || state.touchEnd || !state.touchStart) return;
+		state.touchMove = true;
 		var currentX = e.type === 'touchmove' ? e.targetTouches[0].pageX : e.pageX;
         var currentY = e.type === 'touchmove' ? e.targetTouches[0].pageY : e.pageY;
 
-		if(state.direction === 'x'){
-			state.translateX = Math.round((currentX - state.startX) * state.resistanceRatio);
-			ic.translate(state.translateX, 0, 0);
-        }else if(state.direction === 'y'){
-        	state.translateY = Math.round((currentY - state.startY) * state.resistanceRatio);
-        	ic.translate(0, state.translateY, 0);
+		if(state.currStore.direction === 'x'){
+			state.diffX = Math.round((currentX - state.startX) * state.currStore.touchRatio);
+			ic.translate(state.diffX + state.currStore.translateX, 0, 0);
+        }else{
+        	state.diffY = Math.round((currentY - state.startY) * state.currStore.touchRatio);
+        	ic.translate(0, state.diffY + state.currStore.translateY, 0);
         }
 	};
+
 	ic.touchEnd = function(e){
-        ic.translateEnd();
-        state = Object.create(null);
+		state.touchEnd = true;
+		if(!state.touchStart || !state.touchMove) return;
+		if((e.timeStamp - state.startTime) < 200){
+			console.log('200ms')
+		   ic.recover(state.currStore.translateX, state.currStore.translateY, 0);
+		}else{
+			console.log('touchEnd')
+			var index = state.currStore.index;
+			if(Math.abs(state.diffX) < state.currStore.limitDisX && Math.abs(state.diffY) < state.currStore.limitDisY) {
+				console.log('未到界限')
+				ic.recover(state.currStore.translateX, state.currStore.translateY, 0);
+			}else{
+				if(state.diffX > 0 || state.diffY > 0) {
+					console.log('上一页')
+					ic.moveTo('preSibling');
+				}else{
+					console.log('下一页')
+					ic.moveTo('nextSibiling');
+				}
+			}			
+		}
 	};
-	ic.translateEnd = function(){
-		console.log(state.translateX, state.translateY)
-	},
+
+	ic.clearState = function(){
+		state = Object.create(null);
+	};
+
+	ic.moveTo = function(type){
+		var index = type === 'nextSibiling' ? state.currStore.index + 1 : state.currStore.index - 1;
+		var currStore = state.currStore;
+		if(index < state.currStore.childLength && index > -1){
+			ic.setIndex(index);
+			if(state.currStore.direction === 'x'){	
+				ic.recover(-index * state.currStore.childWidth, 0, 0);
+				currStore.translateX = -index * currStore.childWidth;
+			}else{
+				ic.recover(0 , -index * state.currStore.childHeight, 0);
+				currStore.translateY = -index * currStore.childHeight;
+			}
+		}else {
+			ic.recover(state.currStore.translateX , state.currStore.translateY, 0);
+		}
+	};
+	ic.setIndex = function(index){
+		state.currStore.index = index;
+	};
+
+	ic.recover = function(x, y, z){
+		state.animating = true;
+		ic.transitionDuration(state.currStore.animationDuration);
+		ic.translate(x, y, z);
+	};
+
 	ic.translate = function(x, y, z){
 		if (ic.support.transforms3d){
 			ic.transform('translate3d(' + x + 'px, ' + y + 'px, ' + z + 'px)');
@@ -76,14 +140,23 @@ function iceSkating(option){
 		elStyle.webkitTransform = elStyle.MsTransform = elStyle.msTransform = elStyle.MozTransform = elStyle.OTransform = elStyle.transform = transform;
 	};
 
-	ic.transition = function(time){
+	ic.transitionDuration = function(time){
 		var elStyle = state.currentTarget.style;
-		elStyle.webkitTransitionDuration = elStyle.MsTransitionDuration = elStyle.msTransitionDuration = elStyle.MozTransitionDuration = elStyle.OTransitionDuration = elStyle.transitionDuration = time;
+		elStyle.webkitTransitionDuration = elStyle.MsTransitionDuration = elStyle.msTransitionDuration = elStyle.MozTransitionDuration = elStyle.OTransitionDuration = elStyle.transitionDuration = time + 'ms';
+	};
+
+	ic.transitionDurationEndFn = function(){
+		console.log('transitionDurationEnd')
+		ic.transitionDuration(0);
+		ic.clearState();
 	};
 
 	ic.initEvent = function(){
-		var events = ic.support.touch?['touchstart', 'touchmove', 'touchend']:['mousedown','mousemove','mouseup'];
-
+		var events = ic.support.touch ? ['touchstart', 'touchmove', 'touchend']:['mousedown','mousemove','mouseup'];
+		var transitionEndEvents = ['webkitTransitionEnd', 'transitionend', 'oTransitionEnd', 'MSTransitionEnd', 'msTransitionEnd'];
+        for (var i = 0; i < transitionEndEvents.length; i++) {
+            ic.addEvent(container, transitionEndEvents[i], ic.transitionDurationEndFn, false);
+        } 
 		ic.addEvent(container, events[0], ic.touchStart);
 		if(!docTouch){
 		ic.addEvent(document, events[1], ic.touchMove, false);
@@ -105,13 +178,6 @@ iceSkating.prototype = {
 		transforms3d : (function () {
                 var div = document.createElement('div').style;
                 return ('webkitPerspective' in div || 'MozPerspective' in div || 'OPerspective' in div || 'MsPerspective' in div || 'perspective' in div);
-        })(),
-        flexbox: (function () {
-                var div = document.createElement('div').style;
-                var styles = ('alignItems webkitAlignItems webkitBoxAlign msFlexAlign mozBoxAlign webkitFlexDirection msFlexDirection mozBoxDirection mozBoxOrient webkitBoxDirection webkitBoxOrient').split(' ');
-                for (var i = 0; i < styles.length; i++) {
-                    if (styles[i] in div) return true;
-                }
         })()
 	}
 };
